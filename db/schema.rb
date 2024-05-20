eb743a3bb9506205d3584a7aa62b03189891291e
harunanon <haruna.nobuoka@gmail.com>
2024-05-20 14:14:56 +0900

    データベース復元

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_17_091204) do
     t.string "title"
     t.string "catch_copy"
     t.string "concept"
-    t.integer "user_id"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -78,4 +79,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_17_091204) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "prototypes"
   add_foreign_key "comments", "users"
+  add_foreign_key "prototypes", "users"
 end
